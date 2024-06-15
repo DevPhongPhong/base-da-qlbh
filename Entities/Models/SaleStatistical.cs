c8289cdc79aa19bcad93fc697260230242eac3f0
namdaa <nguyennam24102002@gmail.com>
2024-06-15 19:10:25 +0700

    update

--- Entities/Models/SaleStatistical.cs
+++ Entities/Models/SaleStatistical.cs
@@ -11,11 +11,12 @@ namespace Entities.Models
     }
 }
 // -- qlbh.productstatistical definition
-
-// CREATE TABLE `ProductStatistical` (
-//   `Id` int NOT NULL AUTO_INCREMENT,
-//   `Year` int NOT NULL,
-//   `TotalQuantity` int NOT NULL,
-//   `ProductName` varchar(255) NOT NULL,
-//   PRIMARY KEY (`Id`)
-// ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*
+CREATE TABLE `ProductStatistical` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Year` int NOT NULL,
+  `TotalQuantity` int NOT NULL,
+  `ProductName` varchar(255) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+*/
